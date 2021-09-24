cf248db6c926f4ec71d6daab1f47e9d997cf0889
Ananyahaha <ananyam1999@gmail.com>
2021-09-24 23:04:15 +0530

    Completed module

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+public class CelestialObject
+{
+    public int Id;
+    [Required]
+    public string Name;
+    public int OrbitedObjectId;
+    public List<CelestialObject> Satellites;
+    public TimeSpan OrbitalPeriod;
+
+
+
+}
