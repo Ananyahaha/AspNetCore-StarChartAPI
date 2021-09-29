676f9561d013e878919fe64c28a2b8e328131087
Ananyahaha <ananyam1999@gmail.com>
2021-09-29 15:50:39 +0530

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -28,7 +28,7 @@ namespace StarChart.Controllers
         [HttpGet("{name}")]
         public IActionResult GetByName(string name)
         {
-            var celestialObjects = _context.CelestialObjects.ToList();
+            var celestialObjects = _context.CelestialObjects.Where(e=>e.Name==name).ToList();
             foreach(var celestialObject in celestialObjects)
             {
                 celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
